f559624ceab7f6d1f659b65b6316a85653242573
Roman Andriichuk <romanandriichuik17@gmail.com>
2025-04-07 19:57:03 +0200

    Developed Shared and Catalog Data-Infrastructure with EF Core

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
 namespace Catalog
 {
@@ -10,10 +12,30 @@ namespace Catalog
             IConfiguration configuration)
         {
             // Add services to the container.
-            // services
-            //      .AddApplicationServices()
-            //      .AddInfrastuctureServices(configuration)
-            //      .AddApiServices(configuration);
+
+            // Api Endpoint services
+
+            // Application Use Case services
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            });
+
+            // Data - Infrastructure services
+
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+            services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+
+            services.AddDbContext<CatalogDbContext>((sp, options) =>
+            {
+                options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+                options.UseNpgsql(connectionString);
+            });
+
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
 
             return services;
         }
@@ -21,10 +43,15 @@ namespace Catalog
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
             // Configure the HTTP request pipeline.
-            //app
-            //      .UseApplicationServices()
-            //      .UseInfrastructureServices()
-            //      .UseApiServices();
+
+            // 1. Use Api Endpoint services
+
+            // 2. Use Application Use Case services
+
+            // 3. Use Data - Infrastructure services
+
+            app.UseMigration<CatalogDbContext>();
+
             return app;
         }
     }
