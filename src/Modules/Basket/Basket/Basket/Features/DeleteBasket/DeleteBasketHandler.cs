2758eb7aea889455337c996bd32441912731c235
Roman Andriichuk <romanandriichuik17@gmail.com>
2025-11-08 14:56:39 +0100

    Developed Basket Application Use Case Development with CQRS and MediatR

---
+++ src/Modules/Basket/Basket/Basket/Features/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,25 @@
+﻿namespace Basket.Basket.Features.DeleteBasket
+{
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool isSuccess);
+    internal class DeleteBasketHandler(BasketDbContext dbContext) : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            //Delete basket entity from command object
+            //save to database
+            //return result
+
+            var basket = await dbContext.ShoppingCarts.SingleOrDefaultAsync(x => x.UserName == command.UserName, cancellationToken);
+
+            if (basket == null)
+            {
+                throw new BasketNotFoundException(command.UserName);
+            }
+
+            dbContext.ShoppingCarts.Remove(basket);
+            await dbContext.SaveChangesAsync(cancellationToken);
+            return new DeleteBasketResult(true);
+        }
+    }
+}
